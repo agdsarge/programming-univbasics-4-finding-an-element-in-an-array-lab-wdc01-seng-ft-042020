f96ab4e55d76a13d6b7c466d3c9ce6ccb20880d8
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-10 20:14:32 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   index = 0
   while index < array.length do
     if array[index] == value_to_find then return index end
-    if 
+    if index == array.length - 1 then return nil end
     index += 1
   end
 end

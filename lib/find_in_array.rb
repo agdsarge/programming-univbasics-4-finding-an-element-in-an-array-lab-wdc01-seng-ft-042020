ceeb33751eb7d6de7cf10d2d3749317f9a602c49
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-10 20:10:29 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,13 +1,12 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
   array.sort!
-  length = array.length 
-  mid_point = length / 2
+  mid_point = array.length / 2
   
   if array[mid_point] == value_to_find
     return mid_point
   elsif array[mid_point] < value_to_find
-    
+    find_element_index(array.spl)
   else
   end
 end
